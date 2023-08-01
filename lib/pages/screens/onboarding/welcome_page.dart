8fc910a708ac145515f8e97a86be9e42a86d7491
BhreKheley <bhrenabil@gmail.com>
2023-08-01 08:00:27 +0700

    [BN] Ganti bottom navbar

--- lib/pages/screens/onboarding/welcome_page.dart
+++ lib/pages/screens/onboarding/welcome_page.dart
@@ -11,34 +11,42 @@ class WelcomePage extends StatelessWidget {
         child: Column(
           children: [
             Container(
-                padding: EdgeInsets.zero,
-                width: 400,
-                height: 440,
-                child: Stack(
-                  clipBehavior: Clip.none,
-                  children: [
-                    Container(),
-                    Positioned(
-                      bottom: -27,
-                      child: Image.asset(
-                        'assets/img/onboarding/seeding.png',
-                        height: 472,
-                      ),
+              width: MediaQuery.of(context).size.width,
+              height: 440,
+              child: Stack(
+                clipBehavior: Clip.none,
+                children: [
+                  Positioned(
+                    bottom: -27,
+                    child: Image.asset(
+                      'assets/img/onboarding/seeding.png',
+                      height: 472,
                     ),
-                  ],
-                )),
+                  ),
+                ],
+              ),
+            ),
             Padding(
-              padding: const EdgeInsets.only(right: 145, top: 42),
+              padding: const EdgeInsets.only(
+                right: 145,
+                top: 42,
+              ),
               child: Text(
                 "Selamat Datang di \nVariegata",
-                style: TextStyle(fontSize: 26, fontWeight: FontWeight.w700),
+                style: TextStyle(
+                  fontSize: 26,
+                  fontWeight: FontWeight.w700,
+                ),
               ),
             ),
             Padding(
               padding: const EdgeInsets.only(right: 75, top: 16),
               child: Text(
                 "Variegata adalah aplikasi\npetani bahasa indonesia yang dapat\nmembantu anda dalam menanam\ntanaman yang anda inginkan!",
-                style: TextStyle(fontSize: 18, fontWeight: FontWeight.w500),
+                style: TextStyle(
+                  fontSize: 18,
+                  fontWeight: FontWeight.w500,
+                ),
               ),
             ),
           ],
