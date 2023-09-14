ac9eb3f67f91db9f16f5584b80a4c0744f9b249a
Mikaaz <miikaalearn396@gmail.com>
2023-09-14 09:32:22 +0700

    .

---
+++ lib/midtrans/config.dart
@@ -0,0 +1,72 @@
+
+enum Environment { Local, Dev, Prod }
+
+
+
+class Config {
+  static late Map<String, dynamic> _config = _ConfigMap.localConfig;
+
+  static void setEnvironment(Environment env) {
+    switch (env) {
+      case Environment.Local:
+        _config = _ConfigMap.localConfig;
+        break;
+      case Environment.Dev:
+        _config = _ConfigMap.devConfig;
+        break;
+      case Environment.Prod:
+        _config = _ConfigMap.prodConfig;
+        break;
+    }
+  }
+
+  static String getEnvironment() {
+    if (_config == _ConfigMap.prodConfig) {
+      return "prod";
+    } else if (_config == _ConfigMap.devConfig) {
+      return "dev";
+    } else {
+      return "local";
+    }
+  }
+
+  static bool isProd() => _config == _ConfigMap.prodConfig;
+
+  static bool isLocal() => _config == _ConfigMap.localConfig;
+
+  static get baseURL {
+    return _config[_ConfigMap.BASE_URL];
+  }
+
+  static get midtransURL {
+    return _config[_ConfigMap.MIDTRANS_URL];
+  }
+
+  static get midtransClientKey {
+    return _config[_ConfigMap.MIDTRANS_CLIENT_KEY];
+  }
+}
+
+class _ConfigMap {
+  static const BASE_URL = "BASE_URL";
+  static const MIDTRANS_URL = "https://app.sandbox.midtrans.com/snap/snap.js";
+  static const MIDTRANS_CLIENT_KEY = "SB-Mid-client-XsUvkW0DyXiyhOck";
+
+  static Map<String, dynamic> localConfig = {
+    BASE_URL: "http://localhost:5000",
+    MIDTRANS_URL: "https://app.sandbox.midtrans.com/snap/snap.js",
+    MIDTRANS_CLIENT_KEY: "SB-Mid-client-XsUvkW0DyXiyhOck"
+  };
+
+  static Map<String, dynamic> devConfig = {
+    BASE_URL: "http://localhost:5000",
+    MIDTRANS_URL: "https://app.sandbox.midtrans.com/snap/snap.js",
+    MIDTRANS_CLIENT_KEY: "SB-Mid-client-XsUvkW0DyXiyhOck"
+  };
+
+  static Map<String, dynamic> prodConfig = {
+    BASE_URL: "http://localhost:5000",
+    MIDTRANS_URL: "https://app.sandbox.midtrans.com/snap/snap.js",
+    MIDTRANS_CLIENT_KEY: "SB-Mid-client-XsUvkW0DyXiyhOck"
+  };
+}
--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -7,12 +7,14 @@ import Foundation
 
 import geolocator_apple
 import location
+import modal_progress_hud_nsn
 import path_provider_foundation
 import sqflite
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   GeolocatorPlugin.register(with: registry.registrar(forPlugin: "GeolocatorPlugin"))
   LocationPlugin.register(with: registry.registrar(forPlugin: "LocationPlugin"))
+  ModalProgressHudNsnPlugin.register(with: registry.registrar(forPlugin: "ModalProgressHudNsnPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   SqflitePlugin.register(with: registry.registrar(forPlugin: "SqflitePlugin"))
 }
